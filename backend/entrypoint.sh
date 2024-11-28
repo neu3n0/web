eea43ec990e752c8a2c68c1ed78031c23cdc0a2c
Neu3no <m.kniazev@insilicomedicine.com>
2024-11-28 14:24:25 +0400

    init backend

---
+++ backend/entrypoint.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+python3 manage.py makemigrations --no-input
+python3 manage.py migrate --no-input
+# python3 manage.py runserver 0.0.0.0:8000
+gunicorn -w 8 application.wsgi:application -b 0.0.0.0:8000 --reload
